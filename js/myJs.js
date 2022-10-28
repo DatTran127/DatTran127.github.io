960f72f1733cf7563b4eba587bdcc86a65c1fa13
DatTran127 <92715768+DatTran127@users.noreply.github.com>
2022-10-28 07:27:28 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -118,7 +118,7 @@ $(document).ready(function () {
       background: '#fff url("img/iput-bg.jpg")',
       backdrop: `
                     rgba(0,0,123,0.4)
-                    url("img/giphy2.gif")
+                    url("img/cute-cat-love.gif")
                     left top
                     no-repeat
                   `,
